bf63d024021f24982fcbfdfaba8f0171772c2f19
Matteo <ciaomare>
2020-08-13 14:43:41 +0200

    fixed a bug that doubled the last element inserted if the page was reloaded

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,7 @@
-module.exports = function(app,fs,todoUrl,bodyParser){
+module.exports = function(app,fs,todoUrl,bodyParser,chalk){
     
     app.use(bodyParser.urlencoded({ extended: false }))
- 
-    // parse application/json
-    app.use(bodyParser.json())
+    app.use(bodyParser.json()) // parse application/json
 
 
     var readData = fs.readFileSync( todoUrl, "utf8" ); //{ "todo": ["...","..."]}
@@ -11,18 +9,21 @@ module.exports = function(app,fs,todoUrl,bodyParser){
 
     app.get("/", (req,res)=>{                     //when the user access to the page
         res.render("todo", {todoData: todoList.todo});
+        console.log(chalk.green("Risposta alla chiamata GET eseguita con successo"));
     })
 
     app.post("/",(req,res)=>{                          //if the user wants to create a new data
-        todoList.todo.push(req.body.item);                              //push the new user's item into the todo's array
-        fs.writeFile(todoUrl, JSON.stringify(todoList), (err) => {               //write the new json todo
-            if (err) {
-                return console.log(err);
-            }
-        });
-        res.render("todo", {todoData:todoList.todo});                       //render to the page with the new json item
+        if(req.body.item!==todoList.todo[todoList.todo.length-1]){
+            todoList.todo.push(req.body.item);                              //push the new user's item into the todo's array
+            fs.writeFile(todoUrl, JSON.stringify(todoList), (err) => {               //write the new json todo
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        }
+        res.render("todo", {todoData:todoList.todo});   //render to the page with the new json item
     })
-    //
+                                        
     //if the user wants to delete a data
     app.delete("/assets/todoList.json", (req,res)=>{
         let objectToDelete = req.body.item;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var express = require("express");
 const fs = require("fs");
+const chalk = require("chalk");
 var app = express();
 var bodyParser = require('body-parser'); //to parse the body of the request
 var todoUrl = __dirname + "/public/assets/todoList.json";
-
-// support parsing of application/json type post data
-
-//support parsing of application/x-www-form-urlencoded post data
-
 
 var todoController = require("./controllers/todoController.js")
 
@@ -21,7 +17,7 @@ app.use(express.static("./public"));
 
 //I call the imported function with parameter "app" so it can perform any function of express, fs for filesystem,
 //urlencodeParser to handle the req body and todoUrl for the url of todoList.json 
-todoController(app,fs,todoUrl,bodyParser);
+todoController(app,fs,todoUrl,bodyParser,chalk);
 
 
 app.listen(3000, ()=>{
